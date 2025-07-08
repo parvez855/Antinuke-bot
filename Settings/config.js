aef9c87bda6715e34bb867243df09e1ed3830b60
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-09 01:04:29 +0600

    config.js

--- Settings/config.js
+++ Settings/config.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 module.exports = {
     webhook: {
-        error: "" || process.env.error, //webhook link where error logs are sent
-        join: "" || process.env.join, //webhook link where join logs are sent
-        leave: "" || process.env.leave, //webhook link where leave logs are sent
+        error: "https://discord.com/api/webhooks/1392219021968933035/nSVOYhwW_2iXaZXqly-tDbNL-MGCf8rtrF6GFIkFgKcZ0IUMy2SdqKTibUFhkCoJ1PCQ" || process.env.error, //webhook link where error logs are sent
+        join: "https://discord.com/api/webhooks/1392219021968933035/nSVOYhwW_2iXaZXqly-tDbNL-MGCf8rtrF6GFIkFgKcZ0IUMy2SdqKTibUFhkCoJ1PCQ" || process.env.join, //webhook link where join logs are sent
+        leave: "https://discord.com/api/webhooks/1392219021968933035/nSVOYhwW_2iXaZXqly-tDbNL-MGCf8rtrF6GFIkFgKcZ0IUMy2SdqKTibUFhkCoJ1PCQ" || process.env.leave, //webhook link where leave logs are sent
     },
 
     TOKEN: "" || process.env.TOKEN, //Your Bot Token
-    MONGO_URI: "" || process.env.MONGO_URI, //Mongo Uri
+    MONGO_URI: "mongodb+srv://vox:vox@cluster0.hpjyuwr.mongodb.net/?retryWrites=true&w=majority" || process.env.MONGO_URI, //Mongo Uri
     EMBED_COLOR: "" || process.env.EMBED_COLOR, //Embed Color
-    DEV_ID: "" || process.env.DEV_ID, //Developer ID
-    OWNER_ID: "" || process.env.OWNER_ID, //Owner ID
+    DEV_ID: "1355170681330991145" || process.env.DEV_ID, //Developer ID
+    OWNER_ID: "927208085515280425" || process.env.OWNER_ID, //Owner ID
 
 }
